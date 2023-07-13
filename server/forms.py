ac39d1768a9ec749b76ea8ddacc7ed99cf371968
none <brogliendebele@wesleyan.edu>
2023-07-13 15:23:08 -0400

    merging data

---
+++ server/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+
+class NewsArticleForm(FlaskForm):
+    title = StringField('Title', validators=[
+                        DataRequired(), Length(min=2, max=40)])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    image = StringField('Image', validators=[DataRequired()])
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log in')
